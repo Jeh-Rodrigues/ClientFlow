84b8f2f601571a2412be4e6d58031ab579854805
Jéssica Rodrigues <jessica.rodrigues@kaspper.com>
2026-05-11 22:53:40 -0300

    Initial commit

---
+++ src/main/java/com/clientflow/api/ClientflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.clientflow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientflowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clientflow/api/ClientflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientflow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
